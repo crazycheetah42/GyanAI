3aa2b3eaa2eff8aea47016b9ac31908a5368540c
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-04-12 16:26:42 -0400

    Update GyanSearch.py

--- GyanSearch.py
+++ GyanSearch.py
@@ -1,156 +1,150 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-import speech_recognition as sr
-import pyttsx3
+import tkinter as tk
+from tkinter import ttk
 import openai
+import speech_recognition as sr
 
-openai.api_key = "YOUR_API_KEY_FROM_OPENAI"
+openai_api_key = open("key.txt", "r").read().strip()
+if openai_api_key == "":
+    window = tk.Tk()
+    window.geometry("630x430")
+    window.iconbitmap("search.ico")
+    window.wm_title("OpenAI API key")
 
-class Ui_MainWindow(object):
-    def image_search(self):
-        self.textEdit.setText("Searching...")
-        prompt = self.textEdit.toPlainText()
-        response= openai.Image.create (prompt=prompt, n=1, size="1024x1024")
-        image_url = response['data'][0]['url']
+    label = ttk.Label(window, text="OpenAI API key", font=("Segoe UI", 22))
+    label.pack()
+
+    space_lbl = ttk.Label(window, text="")
+    space_lbl.pack()
+
+    label2 = ttk.Label(window, text="An OpenAI API key is required to proceed. Proceeding without an API key will cause errors while using the app.")
+    label2.pack()
+
+    space_lbl2 = ttk.Label(window, text="")
+    space_lbl2.pack()
+    space_lbl3 = ttk.Label(window, text="")
+    space_lbl3.pack()
+
+    label3 = ttk.Label(window, text="Enter OpenAI API key here:")
+    label3.pack()
+    space_lbl4 = ttk.Label(window, text="")
+    space_lbl4.pack()
+
+    var = tk.StringVar()
+    new_api_key = ttk.Entry(window, textvariable=var, width=75)
+    new_api_key.pack()
+
+    def write_api_key_to_file():
+        api_key = var.get()
+        key_file = open("key.txt", "w")
+        key_file.write(api_key)
+        window.destroy()
+        from tkinter import messagebox
+        messagebox.showinfo("API key added", "The OpenAI API key has been added. You may now restart the program.")
+
+    space_lbl5 = ttk.Label(window, text="")
+    space_lbl5.pack()
+    button = ttk.Button(window, text="OK", command=write_api_key_to_file)
+    button.pack()
+
+    space_lbl6 = ttk.Label(window, text="")
+    space_lbl6.pack()
+    space_lbl7 = ttk.Label(window, text="")
+    space_lbl7.pack()
+
+    link = ttk.Label(window, text="See how to generate an API key", cursor="hand2")
+    link.pack()
+    def open_link(url):
         import webbrowser
-        webbrowser.open(image_url)
-    def search(self):
-        self.textEdit.setText("Searching...")
-        text = self.textEdit.toPlainText()            
+        webbrowser.open(url)
+    link.bind("<Button-1>", lambda e: open_link("https://www.google.com"))
+    link.config(foreground='blue')
+    
+    window.mainloop()
+else:
+    openai.api_key = openai_api_key
 
-        # Send the text to the ChatGPT API and receive a response
-        prompt = (f"User: {text}\n"
-                    f"ChatGPT: ")
-        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=1024, top_p=1, frequency_penalty=0, presence_penalty=0)
-        response_text = response['choices'][0]['text']
-
-        # Convert the ChatGPT response to speech and play it back to the user
-        self.textBrowser.setText(response_text)
-    def speak(self):
-        self.textEdit.setText("Listening...")
-         # Set up the ChatGPT API client
-
-        # Initialize the text-to-speech engine
-        engine = pyttsx3.init()
-        
-        
-        # Record the user's voice input
-        
+    def voice():
         r = sr.Recognizer()
         with sr.Microphone() as source:
             audio = r.listen(source)
 
-        # Transcribe the audio to text
         try:
             text = r.recognize_google(audio)
-            if text == "exit":
-                import sys
-                sys.exit()
-            self.textEdit.setText(f"{text}")
-        except Exception as e:
-            print("Sorry, I wasn't able to understand.")
-            
-
-        # Send the text to the ChatGPT API and receive a response
-        prompt = (f"User: {text}\n"
+            prompt.insert(0, text)
+            text_prompt = (f"User: {text}\n"
                     f"ChatGPT: ")
-        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=1024, top_p=1, frequency_penalty=0, presence_penalty=0)
+            response = openai.Completion.create(engine="text-davinci-003", prompt=text_prompt, temperature=0.5, max_tokens=1024, top_p=1, frequency_penalty=0, presence_penalty=0)
+            response_text = response['choices'][0]['text']
+
+            answer.delete(1.0, tk.END)
+            answer.insert(1.0, response_text)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    def search():
+        text = var.get()
+        
+        text_prompt = (f"User: {text}\n"
+                    f"ChatGPT: ")
+        response = openai.Completion.create(engine="text-davinci-003", prompt=text_prompt, temperature=0.5, max_tokens=1024, top_p=1, frequency_penalty=0, presence_penalty=0)
         response_text = response['choices'][0]['text']
 
-        # Convert the ChatGPT response to speech and play it back to the user
-        self.textBrowser.setText(response_text)
-        engine.say(response_text)
-        engine.runAndWait()
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(360, 800)
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
-        self.centralwidget.setSizePolicy(sizePolicy)
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(112, 10, 120, 51))
-        font = QFont()
-        font.setPointSize(15)
-        self.label.setFont(font)
-        self.textEdit = QTextEdit(self.centralwidget)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(10, 60, 241, 30))
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setStyleSheet("background-color: transparent;")
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(280, 60, 31, 30))
-        self.pushButton.setCursor(QCursor(Qt.ArrowCursor))
-        icon = QIcon()
-        icon.addFile(u"icons8-microphone-24.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.textBrowser = QTextBrowser(self.centralwidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(10, 110, 341, 681))
-        self.textBrowser.setAutoFillBackground(False)
-        self.textBrowser.setTabChangesFocus(True)
-        self.textBrowser.setUndoRedoEnabled(True)
-        self.textBrowser.setLineWrapColumnOrWidth(0)
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setStyleSheet("background-color: transparent;")
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(250, 60, 31, 30))
-        icon1 = QIcon()
-        icon1.addFile(u"icons8-search-24.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.pushButton_2.setIcon(icon1)
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setStyleSheet("background-color: transparent;")
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(310, 60, 31, 30))
-        self.pushButton_3.setCursor(QCursor(Qt.ArrowCursor))
-        icon2 = QIcon()
-        icon2.addFile(u"Camera-icon.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.pushButton_3.setIcon(icon2)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        answer.delete(1.0, tk.END)
+        answer.insert(1.0, response_text)
 
-        # Code section (functionality)
-        
-        self.pushButton.clicked.connect(self.speak)
-        self.pushButton_2.clicked.connect(self.search)
-        self.pushButton_3.clicked.connect(self.image_search)
-        self.retranslateUi(MainWindow)
+    root = tk.Tk()
+    root.geometry("1280x768")
+    root.wm_title("Gyan Search")
+    root.iconbitmap("search.ico")
 
-        QMetaObject.connectSlotsByName(MainWindow)
-    # setupUi
+    header_lbl = ttk.Label(root, text="Gyan Search", font=("Segoe UI", 22))
+    header_lbl.pack()
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Gyan Search", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Gyan Search", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"", None))
-        self.textEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Q:", None))
+    var = tk.StringVar()
 
-if __name__ == "__main__":
-    import darkdetect
-    import qdarkstyle
-    if darkdetect.isDark():
-        import sys
-        app = QApplication(sys.argv)
-        MainWindow = QMainWindow()
-        ui = Ui_MainWindow()
-        ui.setupUi(MainWindow)
-        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        MainWindow.show()
-        sys.exit(app.exec_())
-    else:
-        import sys
-        app = QApplication(sys.argv)
-        MainWindow = QMainWindow()
-        ui = Ui_MainWindow()
-        ui.setupUi(MainWindow)
-        MainWindow.show()
-        sys.exit(app.exec_())
+    space_lbl = ttk.Label(root, text="", font=("Segoe UI", 12))
+    space_lbl.pack()
+
+    input_hint_lbl = ttk.Label(root, text="Enter prompt", font=("Segoe UI", 14))
+    input_hint_lbl.pack()
+
+    prompt = ttk.Entry(root, textvariable=var, width=170)
+    prompt.pack()
+
+    submit_btn = tk.PhotoImage(file="search.png")
+
+    btn_frame = tk.LabelFrame(root)
+    btn_frame.pack()
+
+    submit_button = ttk.Button(btn_frame, image=submit_btn, command=search)
+    submit_button.pack(side="left")
+
+    voice_btn = tk.PhotoImage(file="voice.png")
+
+    voice_button = ttk.Button(btn_frame, image=voice_btn, command=voice)
+    voice_button.pack(side="right")
+
+    space_lbl2 = ttk.Label(root, text="", font=("Segoe UI", 12))
+    space_lbl2.pack()
+    space_lbl3 = ttk.Label(root, text="", font=("Segoe UI", 12))
+    space_lbl3.pack()
+
+    answer = tk.Text(root, height=25, width=120)
+    answer.pack(pady=5)
+
+    space_lbl4 = ttk.Label(root, text="", font=("Segoe UI", 12))
+    space_lbl4.pack()
+    space_lbl5 = ttk.Label(root, text="", font=("Segoe UI", 12))
+    space_lbl5.pack()
+
+    link1 = ttk.Label(root, text="Need help?", cursor="hand2")
+    link1.pack()
+    def open_link(url):
+        import webbrowser
+        webbrowser.open(url)
+    link1.bind("<Button-1>", lambda e: open_link("https://www.google.com"))
+    link1.config(foreground='blue')
+
+    root.mainloop()
