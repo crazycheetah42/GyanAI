a9589e68058255c4bbb6c1807a3f21afbd012b62
Aryaman Sriram <crazycheetah42@outlook.com>
2023-08-22 14:57:22 -0400

    Add LLaMa 2 model to gshorten.py

--- gshorten.py
+++ gshorten.py
@@ -1,12 +1,19 @@
-# Import the necessary modules needed for the GyanAI Chat function
-from bardapi import Bard
+# Import the necessary modules needed for the GyanAI Shorten function
+import replicate, os
 
-# Setup Bard API and store the result of the prompt
-bKey = open("bard.txt", "r").read().strip()
-bard = Bard(token=bKey)
-
+# Setup LLaMa API and add it to the environment variables
+rkey = open("key.txt", 'r').read().strip()
+os.environ["REPLICATE_API_TOKEN"] = rkey
 
 def sShorten(prompt):
-    newPrompt = "Shorten this text without removing the context: \n" + prompt
-    bard_result = bard.get_answer(newPrompt)['content']
-    return bard_result
+    prompt_input = "Shorten this text without removing the context. The shortened text should still convey the meaning of the message. Do not add any extra sentences or explanations, just give the shortened text. Use the text below as reference: \n" + prompt
+    output = replicate.run(
+    "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781",
+    input={"prompt": prompt_input}
+        )
+    full_response = ""
+
+    for item in output:
+        # https://replicate.com/replicate/llama-2-70b-chat/versions/58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781/api#output-schema
+        full_response += item
+    return full_response
