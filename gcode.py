1a51370b57e7273d3e5ce9febd0b3b875102f490
Aryaman Sriram <crazycheetah42@outlook.com>
2023-08-26 16:52:44 -0400

    Add openai txt file for api key.

--- gcode.py
+++ gcode.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = 'YOUR_API_KEY'
+openai.api_key = open("openai.txt", "r").read().strip()
 messages = [ {"role": "system", "content": 
               "You are a intelligent assistant that helps in code generation. When prompted, generate and output only the code. Do not give any explanations or additional answers that is not code (of the language) specified by the User."} ]
 while True:
