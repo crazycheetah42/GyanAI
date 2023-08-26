c22c88df2a98f20c100a0ed8d57f0fbb497b44f2
Aryaman Sriram <crazycheetah42@outlook.com>
2023-08-26 15:15:56 -0400

    Fix some system inputs in OpenAI code

--- gcode.py
+++ gcode.py
@@ -1,7 +1,7 @@
 import openai
 openai.api_key = 'YOUR_API_KEY'
 messages = [ {"role": "system", "content": 
-              "You are a intelligent assistant that helps in code generation. When prompted, generate and output only the code. Do not give any explanations or additional answers that is not code of the language specified by the User."} ]
+              "You are a intelligent assistant that helps in code generation. When prompted, generate and output only the code. Do not give any explanations or additional answers that is not code (of the language) specified by the User."} ]
 while True:
     message = input("User : ")
     if message:
